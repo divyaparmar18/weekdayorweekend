568dfd110f88933c87c98d41540c4e09689090c1
divyaparmar18 <divya18@navgurukul.org>
2019-05-03 23:47:22 +0530

    taking day from user

---
+++ WeekendOrWeekday.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class WeekendOrWeekday {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter the day");
+        String day = obj.nextLine();
+    }
+}
