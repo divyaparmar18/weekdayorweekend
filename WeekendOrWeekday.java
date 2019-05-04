6795739012eab31c0023bb239633548fa7c1943e
divyaparmar18 <divya18@navgurukul.org>
2019-05-04 08:54:26 +0530

    comparing userinput

--- WeekendOrWeekday.java
+++ WeekendOrWeekday.java
@@ -3,6 +3,9 @@ public class WeekendOrWeekday {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("enter the day");
-        String day = obj.nextLine();
+        String day = obj.next();
+        if(day.equals("monday")||(day.equals("MONDAY"))) {
+            System.out.println("The day you entered is a weekday");
+        }
     }
 }
