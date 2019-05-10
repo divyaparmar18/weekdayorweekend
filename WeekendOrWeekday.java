7315507644be9ef2ca3cce598ba982be9fd7befa
divya <divya18@navgurukul.org>
2019-05-10 08:47:41 +0530

    refactor . or conditions to function

--- WeekendOrWeekday.java
+++ WeekendOrWeekday.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
 public class WeekendOrWeekday {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("enter the day");
-        String day = obj.next();
-        if(day.equalsIgnoreCase("monday")||(day.equalsIgnoreCase("tuesday"))||(day.equalsIgnoreCase("wednesday"))||(day.equalsIgnoreCase("thursday"))||(day.equalsIgnoreCase("friday"))) {
+        String day = args[0];
+        if(isaWeekday(day)) {
             System.out.println("The day you entered is a weekday, so you have to work");
         }
-        else if (day.equalsIgnoreCase("saturday")||(day.equalsIgnoreCase("sunday"))){
+        else if (isaWeekend(day)){
             System.out.println("Yeah! the day you entered is weekend, you can take rest");
         }
         else{
             System.out.println("invalid in put");
         }
+    }
+
+    private static boolean isaWeekend(String day) {
+        return day.equalsIgnoreCase("saturday")||(day.equalsIgnoreCase("sunday"));
+    }
+
+    private static boolean isaWeekday(String day) {
+        return day.equalsIgnoreCase("monday")||(day.equalsIgnoreCase("tuesday"))||(day.equalsIgnoreCase("wednesday"))||(day.equalsIgnoreCase("thursday"))||(day.equalsIgnoreCase("friday"));
     }
 }
