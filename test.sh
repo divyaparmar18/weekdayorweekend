4c8c9d06dcb72ce0cc8549e168bc94aff9904538
divya <divya18@navgurukul.org>
2019-05-10 08:52:11 +0530

    add . testcases for weekdays

---
+++ test.sh
@@ -0,0 +1,16 @@
+javac WeekendOrWeekday.java
+java  WeekendOrWeekday monday > actual
+diff actual expectWeekdays
+java  WeekendOrWeekday tuesday > actual
+diff actual expectWeekdays
+java  WeekendOrWeekday wednesday > actual
+diff actual expectWeekdays
+java  WeekendOrWeekday thursday > actual
+diff actual expectWeekdays
+java  WeekendOrWeekday friday > actual
+diff actual expectWeekdays
+
+
+
+
+
